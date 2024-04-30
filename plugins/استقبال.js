3d7ef8283bb510eeb235b8fde549cb5dd1e941e5
Vermila <168120256+Vermila@users.noreply.github.com>
2024-04-30 03:56:59 +0100

    Create استقبال.js

---
+++ plugins/استقبال.js
@@ -0,0 +1,29 @@
+let handler = async (m, { conn }) => {
+    let user = global.db.data.users[m.sender];
+    let name = conn.getName(m.sender);
+    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
+    let message = `*〄━━━━━━━⌬〔🏮〕⌬━━━━━━━〄*
+
+*⌝ رابط خاصه بالستقبال بالنقابة〣🍷⌞*
+
+ـ· • • ━━━━━━ ⌞⚜️⌝ ━━━━━━ • • ·
+*⌝نرحب بكم في نقابة مافيا 〣🍷⌞*
+ـ· • • ━━━━━━ ⌞⚜️⌝ ━━━━━━ • • ·
+
+*❐رابط⛩️↶*
+
+*⌝🛕╎جروب الاستقبال⌞*
+*➥【https://chat.whatsapp.com/JwS5RhQayeY4nT1yYbaRTm 】*
+
+*〄━━━━━━━⌬〔🏮〕⌬━━━━━━━〄*
+ تــــوقــيــع ↯:
+*⌞ LEVI‹⚜️›YORUICHI ⌝* `;
+    
+
+    conn.sendFile(m.chat, 'https://telegra.ph/file/1ee2af6fdf7c01918916b.jpg', 'image.jpg', message, m);
+};
+
+handler.customPrefix = /^(استقبال)$/i;
+handler.command = new RegExp;
+
+export default handler;
