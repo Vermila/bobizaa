3a98c8fbb84c286b69d31a1edab4f638adefc82b
Vermila <168120256+Vermila@users.noreply.github.com>
2024-05-14 19:13:06 +0100

    Create استماره.js

---
+++ plugins/استماره.js
@@ -0,0 +1,17 @@
+let handler = async (m, { conn }) => {
+    let message = 'هذا الامر قيد التعديل 🎗️🧡';
+
+    // رابط الملف الصوتي
+    let audioUrl = 'https://a.uguu.se/wwXYNGLZ.mp3';
+
+    // إرسال الصورة مع الرسالة
+    await conn.sendFile(m.chat, 'https://telegra.ph/file/1ee2af6fdf7c01918916b.jpg', 'image.jpg', message, m);
+
+    // إرسال الرسالة الصوتية
+    await conn.sendMessage(m.chat, { audio: { url: audioUrl }, mimetype: 'audio/mpeg', ptt: true }, { quoted: m });
+};
+
+handler.customPrefix = /^(استماره)$/i;
+handler.command = new RegExp;
+
+export default handler;
